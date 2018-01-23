256105351cace95e157670bd8391487fde02201a
fitumi <fitumi@users.noreply.github.com>
2018-01-23 21:32:17 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
-
+    printf(
+        R"( <meta charset="utf-8">
+            <style type="text/css">
+              .white.king:before   { content: "\2654";  }
+            </style>)");
     return 0;
 }
